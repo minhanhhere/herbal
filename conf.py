c6360b04829ff94f2b9570947081baaad794d14d
minhanhhere <minhanhhere@gmail.com>
2020-04-27 23:30:09 +0700

    add extension

--- conf.py
+++ conf.py
@@ -29,7 +29,11 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+  "sphinx_search.extension",
+  "sphinx_tabs.tabs",
+  "sphinx.ext.imgmath",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
