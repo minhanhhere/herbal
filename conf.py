89e8afdf002b227502ce2271ae7eeb490bb288c7
minhanhhere <minhanhhere@gmail.com>
2020-04-27 23:41:11 +0700

    update info

--- conf.py
+++ conf.py
@@ -48,8 +48,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Medical Herbal'
-copyright = u'2020, M@rs'
+project = u'Herbal Medicine'
+copyright = u'2020, minhanhhere'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
